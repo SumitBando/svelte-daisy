b7cbc00b694bd7adedd750f1faf1f5d9bf7e63ea
Sumit Bando <sumit@hotmail.com>
2021-11-28 20:01:17 +0530

    Document a button with icon

--- src/routes/buttonsamples.svelte
+++ src/routes/buttonsamples.svelte
@@ -19,3 +19,9 @@ Styles
 <Button btn-xs btn-outline>XSmall outline</Button><br>
 <div class="divider"/> 
 <Button btn-wide btn-outline btn-error>Wide error</Button>
+<div class="divider"/> 
+Button with icon<br><br>
+<Button>
+  <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+  <span>Download</span>
+</Button>
